71d488775340758db20e94596e6b21ba526f4095
ShitalAPatil <shitalpatilb29@gmail.com>
2023-01-21 21:24:53 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+let obj1=[
+    {
+    question: "What is a dog",
+    answer : "Dog is a animal with 4 legs",
+    imageLink : "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dreamstime.com%2Fphotos-images%2Fdog.html&psig=AOvVaw1AszisngszJrfeNhNPN9ZL&ust=1674388244063000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCKCfm9bM2PwCFQAAAAAdAAAAABAE",
+
+},
+{
+    question: "What is a moon",
+    answer : "The Moon is Earth's only natural satellite.",
+    imageLink : "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thoughtco.com%2Fwhat-is-the-moon-made-of-604005&psig=AOvVaw0hHLoldtRcNEs3Ogv9EsQH&ust=1674388342460000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKjB4IXN2PwCFQAAAAAdAAAAABAE",
+
+},
+{
+
+    question: "What is a mitochondria",
+    answer : "It is powerhouse of cell.",
+    imageLink : "https://www.google.com/imgres?imgurl=https%3A%2F%2Fbiologydictionary.net%2Fwp-content%2Fuploads%2F2017%2F03%2FAnimal-mitochondrion-diagram.jpg&imgrefurl=https%3A%2F%2Fbiologydictionary.net%2Fmitochondria%2F&tbnid=XQIWLVrKlNqEPM&vet=12ahUKEwiEv7GMzdj8AhWoLrcAHe9xAccQMygFegUIARDtAQ..i&docid=dsMbg0TZ-fXRbM&w=582&h=399&q=mitochondria&ved=2ahUKEwiEv7GMzdj8AhWoLrcAHe9xAccQMygFegUIARDtAQ",
+
+
+}
+]
+
+
+
+
+
